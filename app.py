6a962a35e2bfd02f5e6a376ede18654d282bb7dd
Euris Liendo <eeliendoc@gmail.com>
2023-08-17 21:57:02 -0600

    crear vista de estudiantes

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
 @app.route("/")
 def index():
-    return "hello world!"
+    return render_template("index.html")
+
+@app.route("/estudiantes")
+def estudiantes():
+    data = [
+        {"Matricula": "23007", "Nombre": "Euris", "Apellido_Paterno": "Liendo", "Apellido_Materno": "Cordero"},
+        {"Matricula": "23008", "Nombre": "Fernando", "Apellido_Paterno": "Lozano", "Apellido_Materno": "Martínez"},
+        {"Matricula": "23009", "Nombre": "Cecilia", "Apellido_Paterno": "Ramos", "Apellido_Materno": "León"},
+        {"Matricula": "23010", "Nombre": "Alessandra", "Apellido_Paterno": "Bernardo", "Apellido_Materno": "Moreno"}
+    ]
+    return render_template("estudiantes.html", data = data)
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=8888)
-    
