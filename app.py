93429f87fe3efe6a30029747aa3530d8a36393d2
Euris Liendo <eeliendoc@gmail.com>
2023-08-21 16:02:50 -0600

    actualizacion

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from entidades.estudiantes import Estudiante
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,13 +8,17 @@ def index():
 
 @app.route("/estudiantes")
 def estudiantes():
-    data = [
-        {"Matricula": "23007", "Nombre": "Euris", "Apellido_Paterno": "Liendo", "Apellido_Materno": "Cordero"},
-        {"Matricula": "23008", "Nombre": "Fernando", "Apellido_Paterno": "Lozano", "Apellido_Materno": "Martínez"},
-        {"Matricula": "23009", "Nombre": "Cecilia", "Apellido_Paterno": "Ramos", "Apellido_Materno": "León"},
-        {"Matricula": "23010", "Nombre": "Alessandra", "Apellido_Paterno": "Bernardo", "Apellido_Materno": "Moreno"}
-    ]
-    return render_template("estudiantes.html", data = data)
+    estudiantes = []
+    with open("db/estudiantes.txt", "r") as f:
+        lines = f.read().splitlines()
+        for l in lines:
+            data = l.split("|")
+            estudiantes.append(Estudiante(data[0], data[1], data[2], data[3]))
+
+    return render_template("estudiantes.html", data=estudiantes)
+
+
+
 
 
 if __name__ == '__main__':
