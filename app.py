4178f0d2e58d392ea912242c87223722069e4f61
Euris Liendo <eeliendoc@gmail.com>
2023-08-24 13:32:41 -0600

    agregando profesores

--- app.py
+++ app.py
@@ -34,5 +34,20 @@ def materias():
     return render_template("materias.html", materias=materias)
 
 
+from entidades.profesores import Profesores
+@app.route("/profesores")
+def profesores():
+    profesores = []
+    with open("db/profesores.txt", "r") as f:
+        lines = f.read().splitlines()
+        for l in lines:
+            data = l.split("|")
+            profesores.append(Profesores(data[0], data[1], data[2], data[3]))
+            print(profesores)
+            
+    return render_template("profesores.html", profesores=profesores)
+
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=8888)
---
+++ entidades/profesores.py
@@ -0,0 +1,7 @@
+class Profesores : 
+    def __init__(self, nombre, matricula, materia, salón):
+        self.nombre = nombre
+        self.matricula = matricula
+        self.materia = materia
+        self.salón = salón
+       
